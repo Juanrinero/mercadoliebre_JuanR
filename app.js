98a6bc0cb70615a75bd2b366c591f2b4872f7e69
juan Rinero <juanrinero@gmail.com>
2022-03-15 22:52:39 -0300

    s

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.listen(PORT,() => {
 });
 
 
-app.get("/",(req,res) => {res.sendFile(path.join(__dirname,"Views","home.html"))})
-app.get("/register",(req,res) => {res.sendFile(path.join(__dirname,"Views","register.html"))})
-app.get("/login",(req,res) => {res.sendFile(path.join(__dirname,"Views","login.html"))})
+app.get("/",(req,res) => {res.sendFile(path.join(__dirname,"views","home.html"))})
+app.get("/register",(req,res) => {res.sendFile(path.join(__dirname,"views","register.html"))})
+app.get("/login",(req,res) => {res.sendFile(path.join(__dirname,"views","login.html"))})
