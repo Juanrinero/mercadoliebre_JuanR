d45a2a24fffe3ea2cd2ce760e19276277ded2663
juan Rinero <juanrinero@gmail.com>
2022-03-15 22:26:46 -0300

    se cambio configuracion de puerto

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require("path");
 
 const publicPath = path.resolve(__dirname,"public");
 
+const PORT = process.env.PORT || 3030;
+
 app.use(express.static(publicPath));
 
-app.listen(3030,() => {
+app.listen(PORT,() => {
     console.log("Servidor corriendo en el puerto 3030");
 });
 
